ffa0c7efaf6849ce1a7d2b08bcc1f5a5cbd23339
Aries <urchinzhou@gmail.com>
2024-03-12 15:22:16 +0800

    Render note form and update routes

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,19 +1,23 @@
 const config = {
-    // Backend config
-    s3: {
+  STRIPE_KEY:
+    "pk_test_51Ot1dWRwvL7vxsTi5cdueO8ejFAd5oUKRVeeUmegDx65SUactwI7qsRoIbac2sMlhR319NKDgDJgcABkH38ouKYq00uCUuahxi",
+  // Frontend config
+  MAX_ATTACHMENT_SIZE: 5000000,
+  // Backend config
+  s3: {
     REGION: import.meta.env.VITE_REGION,
     BUCKET: import.meta.env.VITE_BUCKET,
-},
-apiGateway: {
+  },
+  apiGateway: {
     REGION: import.meta.env.VITE_REGION,
     URL: import.meta.env.VITE_API_URL,
-},
-cognito: {
+  },
+  cognito: {
     REGION: import.meta.env.VITE_REGION,
     USER_POOL_ID: import.meta.env.VITE_USER_POOL_ID,
     APP_CLIENT_ID: import.meta.env.VITE_USER_POOL_CLIENT_ID,
     IDENTITY_POOL_ID: import.meta.env.VITE_IDENTITY_POOL_ID,
-},
+  },
 };
 
 export default config;
--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Stack from "react-bootstrap/Stack";
 import { Auth } from "aws-amplify";
-import { useNavigate } from "react-router-dom";
 import "./Login.css";
 import { useAppContext } from "../lib/contextLib";
 import { onError } from "../lib/errorLib";
@@ -10,7 +9,6 @@ import { useFormFields } from "../lib/hooksLib";
 import LoaderButton from "../components/LoaderButton.tsx";
 
 export default function Login() {
-  const nav = useNavigate();
   const { userHasAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(false);
   const [fields, handleFieldChange] = useFormFields({
@@ -30,7 +28,6 @@ export default function Login() {
     try {
       await Auth.signIn(fields.email, fields.password);
       userHasAuthenticated(true);
-      nav("/");
     } catch (error) {
       onError(error);
       setIsLoading(false);
