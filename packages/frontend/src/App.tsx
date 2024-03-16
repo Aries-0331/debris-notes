a0d05df97f6a5f21671ee103bc4edb26baeff1b5
Aries <urchinzhou@gmail.com>
2024-03-16 23:30:57 +0800

    update homepage layout

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import Navbar from "react-bootstrap/Navbar";
 import Links from "./Routes.tsx";
-import Nav from "react-bootstrap/Nav";
+import { Container, Row, Col, Nav, Tab, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { Auth } from "aws-amplify";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext, AppContextType } from "./lib/contextLib";
 import { onError } from "./lib/errorLib";
-import { NavDropdown } from "react-bootstrap";
 
 function App() {
   const nav = useNavigate();
@@ -40,48 +39,65 @@ function App() {
   }
   return (
     !isAuthenticating && (
-      <div className="App container py-3">
-        <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
-          <LinkContainer to="/">
-            <Navbar.Brand className="fw-bold text-muted">Debris</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Nav activeKey={window.location.pathname}>
-              {user.isAuthenticated ? (
-                <>
-                  <NavDropdown title={user.email} id="basic-nav-dropdown">
-                    <LinkContainer to="/settings">
-                      <NavDropdown.Item>Settings</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Item onClick={handleLogout}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </>
-              ) : (
-                <>
-                  <LinkContainer to="/signup">
-                    <Nav.Link>Signup</Nav.Link>
-                  </LinkContainer>
-                  <LinkContainer to="/login">
-                    <Nav.Link>Login</Nav.Link>
-                  </LinkContainer>
-                </>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <AppContext.Provider
-          value={
-            {
-              user,
-              setUser,
-            } as AppContextType
-          }
-        >
-          <Links />
-        </AppContext.Provider>
+      <div className="box">
+        <Container fluid className="flex-container">
+          <Row>
+            <Col>
+              <Navbar
+                collapseOnSelect
+                bg="light"
+                expand="md"
+                className="mb-3 px-3"
+              >
+                <LinkContainer to="/">
+                  <Navbar.Brand className="fw-bold text-muted">
+                    Debris
+                  </Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                  <Nav activeKey={window.location.pathname}>
+                    {user.isAuthenticated ? (
+                      <>
+                        <NavDropdown title={user.email} id="basic-nav-dropdown">
+                          <LinkContainer to="/settings">
+                            <NavDropdown.Item>Settings</NavDropdown.Item>
+                          </LinkContainer>
+                          <NavDropdown.Item onClick={handleLogout}>
+                            Logout
+                          </NavDropdown.Item>
+                        </NavDropdown>
+                      </>
+                    ) : (
+                      <>
+                        <LinkContainer to="/signup">
+                          <Nav.Link>Signup</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                          <Nav.Link>Login</Nav.Link>
+                        </LinkContainer>
+                      </>
+                    )}
+                  </Nav>
+                </Navbar.Collapse>
+              </Navbar>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <AppContext.Provider
+                value={
+                  {
+                    user,
+                    setUser,
+                  } as AppContextType
+                }
+              >
+                <Links />
+              </AppContext.Provider>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   );
